8c7365d9dc28d83ff7f709781987dc7328e7f8e9
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-06-30 13:51:28 -0700

    Write a C program to Standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints a string to standard error
+ * Return: 0 - SUCCESS
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
+}
