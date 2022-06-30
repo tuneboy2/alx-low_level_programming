94c6f591268c108c30a15686945b1582d8afa9ed
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-06-30 03:18:47 -0700

    Print a string with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print a string using printf
+ * Return: 0 - SUCCESS
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
