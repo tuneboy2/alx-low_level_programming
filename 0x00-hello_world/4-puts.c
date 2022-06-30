cfc0dbbf07820fdf973d011c3dc986451f09fe76
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-06-30 03:11:43 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C program that prints a string using puts
+ *
+ * Return: 0 - SUCCESS
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
