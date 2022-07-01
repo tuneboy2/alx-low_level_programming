36deb5f3b3d30bb91b3ff6d8e02711d3bec80ce9
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-07-01 02:55:58 -0700

    Determine if a Random number is Positive or Negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: This program decides if a random number is positive or negative
+ * Return: 0 - SUCCESS
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+
+	else if (n < 0)
+		printf("%d is negative\n", n);
+
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
