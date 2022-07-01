b29bacd8abe3e33d169a845295167629270707e1
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-07-01 03:25:40 -0700

    Determine the Relationship of a random number with 5

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: This program decides the relationshipof numbers.
+ * Return: 0 - SUCCESS
+ */
+
+int main(void)
+{
+	int n, last_no;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+	last_no = n % 10;
+
+	if (last_no > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_no);
+
+	else if (last_no == 0)
+		printf("Last digit of %d is %d and is zero\n", n, last_no);
+
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_no);
+
+	return (0);
+}
