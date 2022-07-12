c292662630cea51ba26e395a4e50b85bcf1fb708
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-07-12 03:36:50 -0700

    Update Parameter value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that updates the value it points to 98.
+ * @n: Parameter to be updated.
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
