a82086cf68f90d960afb2fcb60cc094afb08384e
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-07-14 02:35:40 -0700

    Concatenate Two Strings with respect to a Number

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,9 +15,7 @@ char *_strcat(char *dest, char *src)
 	while (dest[i] != '\0')
 		i++;
 
-	dest[i] = ' ';
-
-	for (i++, j = 0; src[j] != '\0'; j++, i++)
+	for (j = 0; src[j] != '\0'; j++, i++)
 		dest[i] = src[j];
 
 	dest[i] = ('\0');
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - A function that concatenates two strings with respect to a given number.
+ * @dest: Parameter 1
+ * @src: Parameter containing string to be added to Parameter 1
+ * @n: Number controlling length of string to be added.
+ *
+ * Return: A pointer to a Character.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; src[j] != '\0' && j < n; i++, j++)
+		dest[i] = src[j];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
