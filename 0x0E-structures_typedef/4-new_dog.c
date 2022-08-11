79612571adce120ed30f2df6089eac853236010f
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-08-11 03:43:35 -0700

    Create a New dog_t variable by function

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new variable of type dog_t
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ *
+ * Return: A pointer to a type dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t tmp;
+	dog_t *new = &tmp;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+
+	return (new);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 /**
  * struct dog - Declares a Data Type termed dog
  * @name: Name of the dog
@@ -20,5 +22,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
