936d0b3ce95721ec1d552b4f81343c47e55e708a
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-08-11 02:22:19 -0700

    Fix Betty Errors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-struct dog {
+/**
+ * struct dog - Declares a Data Type termed dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
