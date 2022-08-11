aa54d5a092ea1e8fe5fa76713a82a7d3c3cc3216
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-08-11 02:43:50 -0700

    Handle if NULL is passed

--- 0x0E-structures_typedef/test/1-main.c
+++ 0x0E-structures_typedef/test/1-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -8,9 +9,8 @@
  */
 int main(void)
 {
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    
+    init_dog(NULL, "Poppy", 3.5, "Bob");
+    printf("Ok!\n");
     return (0);
 }
