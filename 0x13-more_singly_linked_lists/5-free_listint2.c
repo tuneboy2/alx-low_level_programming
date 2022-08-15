cf1c97ee382bbee9ca2c25ada63eefcdb12d8d92
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-08-15 05:29:47 -0700

    Free List

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Write a function that frees a listint_t list
+ * @head : a pointer to a pointer to the list
+ *
+ * Return: Void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node = *head;
+	listint_t *current;
+
+	while ((current = node) != NULL)
+	{
+		node = node->next;
+		free(current);
+	}
+
+	*head = NULL;
+}
