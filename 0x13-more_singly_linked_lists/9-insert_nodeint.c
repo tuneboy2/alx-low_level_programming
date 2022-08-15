e3be7327b050d3af8bbebf8b9cffa623d665c4a7
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-08-15 08:23:34 -0700

    Insert a node into a list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index -  inserts a new node at a given position.
+ * @head: pointer to a pointer to the list
+ * @idx: index number node should be added to
+ * @n: element to be initialized to the new node
+ *
+ * Return: A pointer to a data type listint_t
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node = *head;
+	listint_t *new_node;
+	unsigned int i;
+
+	/* Handle if Head address is Null but index given is not 0*/
+	if (node == NULL && idx != 0)
+		return (NULL);
+
+	/* Allocate Memory for new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		free(new_node);
+		return (NULL);
+	}
+
+	/* Handle if index given != 0 */
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 && node != NULL; i++)
+		{
+			node = node->next;
+		}
+	}
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = node->next;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = node->next;
+		node->next = new_node;
+	}
+
+	return (new_node);
+}
