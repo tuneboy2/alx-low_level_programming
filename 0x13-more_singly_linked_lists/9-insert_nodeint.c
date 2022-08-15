dc8096c85b9a2d83b4283c11e20752ec9ff7dc98
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-08-15 12:23:14 -0700

    Fix Handling if index == 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new_node->next = node->next;
+		new_node->next = *head;
 		*head = new_node;
 	}
 	else
