3debf678c0896342c24875db2fad2249cce45b19
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-08-21 13:11:55 -0700

    Copy File

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,14 +1,39 @@
 #include "main.h"
 
 /**
- * main - Write a program that copies the content of a file to another file.
+ * error_file - Indicate if an error occurred
+ * @file_from: check if error occurred from this function
+ * @file_to: check if error occurred from this function
+ * @argv: Command Line Argument
  *
- * Return: An integer
+ * Return: void
+ */
+
+void error_file(int file_from, int file_to, char *argv)
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error Can't read from file %s\n", argv);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
+		exit(99);
+	}
+}
+
+/**
+ * main - Write a program that copies the content of a file to another file.
+ * @ac: Command Argument Count
+ * @av: Pointers to Command Argument
+ *
+ * Return: An Integer
  */
 
 int main(int ac, char **av)
 {
-	int fd, fd1, no;
+	int file_from, file_to, nwr, nrd, err_close;
 	char buffer[1024];
 
 	if (ac != 3)
@@ -17,38 +42,31 @@ int main(int ac, char **av)
 		exit(97);
 	}
 
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
+	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	error_file(file_from, file_to, *av);
+
+	nrd = 1024;
+	while (nrd == 1024)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		nrd = read(file_from, buffer, 1024);
+		error_file(nrd, 0, av[1]);
+
+		nwr = write(file_to, buffer, nrd);
+		error_file(0, nwr, av[2]);
 	}
 
-	fd1 = fd;
-
-	no = read(fd, buffer, 1024);
-
-	close(fd);
-	if (fd == -1)
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
-		dprintf(2, "Can't close fd %d\n", fd1);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	fd = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-	if (fd == -1)
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
-		dprintf(2, "Can't write to %s\n", av[2]);
-		exit(99);
-	}
-
-	fd1 = fd;
-	write(fd, buffer, no);
-
-	close(fd);
-	if (fd == -1)
-	{
-		dprintf(2, "Can't close fd %d\n", fd1);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", file_to);
 		exit(100);
 	}
 
