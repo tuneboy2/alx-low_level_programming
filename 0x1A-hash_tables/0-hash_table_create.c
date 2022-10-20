c2f41b6d3f0ac1dd46ce50b35aff4a605eea6ca5
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-20 16:08:38 -0700

    Fixing hash table arrat

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,10 +10,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
+	hash_node_t **hash;
 
-	new_hash = malloc(size * sizeof(hash_table_t));
+	new_hash = malloc(sizeof(hash_table_t));
 	if (!new_hash)
 		return (NULL);
+
+	hash = malloc(size * sizeof(hash_node_t));
+	if(!hash)
+		return (NULL);
+
+	new_hash->size = size;
+	new_hash->array = hash;
 
 	return (new_hash);
 }
