4ee6e31b00730507f38bc87e71c6da019ecc4964
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-21 05:36:12 -0700

    Set element in hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -46,7 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(key, ht->array[index]->key) == 0)
 			{
-				ht->array[index]->value = value;
+				ht->array[index]->value = value_cpy;
 				return (1);
 			}
 			ht->array[index] = ht->array[index]->next;
