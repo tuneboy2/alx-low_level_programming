2d392cad135d46952619af7456ed57562087c853
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-21 01:03:54 -0700

    Create a Hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,18 +10,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
-	hash_node_t **hash;
+	unsigned long int i;
 
 	new_hash = malloc(sizeof(hash_table_t));
 	if (!new_hash)
 		return (NULL);
 
-	hash = malloc(size * sizeof(hash_node_t));
-	if(!hash)
+	new_hash->size = size;
+	new_hash->array =  malloc(size * sizeof(hash_node_t *));
+	if (!new_hash->array)
 		return (NULL);
 
-	new_hash->size = size;
-	new_hash->array = hash;
-
+	for (i = 0; i < size; i++)
+	{
+		new_hash->array[i] = NULL;
+	}
 	return (new_hash);
 }
